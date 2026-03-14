99fb6ea2251823e8654152f4d3bda959a093aadc
Samuel-Robert <samuelroberty95@gmail.com>
2026-03-14 16:30:28 -0300

    first commit

---
+++ src/main/java/dev/java10x/AprendendoSpringBoot/AprendendoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.AprendendoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/AprendendoSpringBoot/AprendendoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.AprendendoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
