79fb6afdf71481f9c7a31909551dcc9a30ad8220
Samuel-Robert <samuelroberty95@gmail.com>
2026-03-15 20:59:53 -0300

    Adicionei e implementei as anotations do Lombok

--- src/main/java/dev/java10x/AprendendoSpringBoot/Carros/CarroModel.java
+++ src/main/java/dev/java10x/AprendendoSpringBoot/Carros/CarroModel.java
@@ -2,9 +2,15 @@ package dev.java10x.AprendendoSpringBoot.Carros;
 
 import dev.java10x.AprendendoSpringBoot.Pessoas.PessoasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_carro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CarroModel {
 
     @Id
@@ -20,54 +26,4 @@ public class CarroModel {
     @JoinColumn(name = "pessoa_id")// foreign key/chave estrangeira
     private PessoasModel pessoa;
 
-    public CarroModel() {
-    }
-
-    public CarroModel(String modelo, String marca, String placa, int ano, int valor) {
-        this.modelo = modelo;
-        this.marca = marca;
-        this.placa = placa;
-        this.ano = ano;
-        this.valor = valor;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getPlaca() {
-        return placa;
-    }
-
-    public void setPlaca(String placa) {
-        this.placa = placa;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
-    public int getValor() {
-        return valor;
-    }
-
-    public void setValor(int valor) {
-        this.valor = valor;
-    }
 }
--- src/main/java/dev/java10x/AprendendoSpringBoot/Pessoas/PessoasModel.java
+++ src/main/java/dev/java10x/AprendendoSpringBoot/Pessoas/PessoasModel.java
@@ -2,10 +2,16 @@ package dev.java10x.AprendendoSpringBoot.Pessoas;
 
 import dev.java10x.AprendendoSpringBoot.Carros.CarroModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //  Entity ele transforma uma classe em uma entidade do BD
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PessoasModel {
 
     @Id
@@ -18,37 +24,4 @@ public class PessoasModel {
     // OnetoMany - uma pessoa pode ter vários carros
     @OneToMany(mappedBy = "pessoa")
     private CarroModel carro;
-
-    public PessoasModel() {
-    }
-
-    public PessoasModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
